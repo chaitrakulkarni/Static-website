b08cf690dfceef223d07bc58fc9d52c7897480e4
chaitrakulkarni <chaitra.kulkarni2@gmail.com>
2022-10-16 17:10:33 +0530

    Static Website

---
+++ javascript/courseDetails.js
@@ -0,0 +1,8 @@
+<script> 
+    function course_details(){
+        console.log("Course Details1")
+        console.log("Course Details1")
+        console.log("Course Details1")
+        console.log("Course Details1")
+    }
+</script>
